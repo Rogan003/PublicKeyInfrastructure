7c4b7d11163b44935f56b181d7b1ce1efa9a6bca
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2025-09-16 20:29:08 +0200

    Added HTTPS config

--- public-key-infrastructure/vite.config.ts
+++ public-key-infrastructure/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as fs from "node:fs";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+        https: {
+            key: fs.readFileSync('./client.key'),
+            cert: fs.readFileSync('./client.crt'),
+        },
+        port: 3000,
+    },
 })
